d833f83090fbb46b945e8274ad22c5d108fe1168
Albar <GalacticHyperStar@gmail.com>
2022-11-24 19:01:03 -0500

    JWT token creation errors.

--- snowflake-connector/src/jwt.rs
+++ snowflake-connector/src/jwt.rs
@@ -1,9 +1,11 @@
 use jwt_simple::prelude::*;
 
-pub fn create_token(account_identifier: &str, user: &str) -> Result<String, anyhow::Error> {
+pub fn create_token(account_identifier: &str, user: &str) -> Result<String, KeyPairError> {
     let private_key = get_private_key()?;
     let public_key_fingerprint = get_public_key()?;
-    let mut public_key_fingerprint = RS256PublicKey::from_pem(&public_key_fingerprint)?.sha256_thumbprint();
+    let mut public_key_fingerprint = RS256PublicKey::from_pem(&public_key_fingerprint)
+        .map_err(KeyPairError::FingerprintGeneration)?
+        .sha256_thumbprint();
     let padding = public_key_fingerprint.len() % 3;
     for _ in 0..padding {
         public_key_fingerprint.push('=');
@@ -13,18 +15,38 @@ pub fn create_token(account_identifier: &str, user: &str) -> Result<String, anyh
     let claims = Claims::create(Duration::from_hours(1))
         .with_issuer(issuer)
         .with_subject(qualified_username);
-    let key_pair = RS256KeyPair::from_pem(&private_key)?;
+    let key_pair = RS256KeyPair::from_pem(&private_key)
+        .map_err(KeyPairError::KayPairGeneration)?;
     key_pair.sign(claims)
+        .map_err(KeyPairError::KayPairGeneration)
 }
 
-fn get_private_key() -> Result<String, anyhow::Error> {
-    let private_key = std::fs::read_to_string("./environment_variables/snowflake_private_key_path.txt")?;
-    Ok(std::fs::read_to_string(private_key)?)
+fn get_private_key() -> Result<String, KeyPairError> {
+    let path = "./environment_variables/snowflake_private_key_path.txt";
+    let private_key = std::fs::read_to_string(path)
+        .map_err(|e| KeyPairError::PrivateKeyRead(e, path.into()))?;
+    std::fs::read_to_string(&private_key)
+        .map_err(|e| KeyPairError::PrivateKeyRead(e, private_key))
 }
 
-fn get_public_key() -> Result<String, anyhow::Error> {
-    let public_key = std::fs::read_to_string("./environment_variables/snowflake_public_key_path.txt")?;
-    Ok(std::fs::read_to_string(public_key)?)
+fn get_public_key() -> Result<String, KeyPairError> {
+    let path = "./environment_variables/snowflake_public_key_path.txt";
+    let public_key = std::fs::read_to_string(path)
+        .map_err(|e| KeyPairError::PublicKeyRead(e, path.into()))?;
+    std::fs::read_to_string(&public_key)
+        .map_err(|e| KeyPairError::PublicKeyRead(e, public_key))
+}
+
+#[derive(thiserror::Error, Debug)]
+pub enum KeyPairError {
+    #[error("failed to read public key, path: {1}—{0}")]
+    PublicKeyRead(std::io::Error, String),
+    #[error("failed to read private key, path: {1}—{0}")]
+    PrivateKeyRead(std::io::Error, String),
+    #[error("failed to generate fingerprint from public key—{0}")]
+    FingerprintGeneration(anyhow::Error),
+    #[error("failed to generate key pair from private key—{0}")]
+    KayPairGeneration(anyhow::Error),
 }
 
 #[cfg(test)]
