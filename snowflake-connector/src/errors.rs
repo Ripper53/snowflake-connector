567be1f046803c641ff4215f0b7986bab7201fb4
Albar <GalacticHyperStar@gmail.com>
2023-01-30 16:49:15 -0500

    Transparent error for better debugging.

--- snowflake-connector/src/errors.rs
+++ snowflake-connector/src/errors.rs
@@ -2,7 +2,7 @@ use crate::jwt::KeyPairError;
 
 #[derive(thiserror::Error, Debug)]
 pub enum SnowflakeError {
-    #[error("could not generate JWT token")]
+    #[error(transparent)]
     Token(#[from] KeyPairError),
     #[error("could not start client—{0}")]
     SqlClient(anyhow::Error),
